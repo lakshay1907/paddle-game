97a64a6754a65a31e94d747aee1f2333b980f516
lakshay1907 <64905086+lakshay1907@users.noreply.github.com>
2020-07-05 23:12:30 +0530

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,55 @@
+class Game {
+  constructor(){}
+  
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+   
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  start(){
+    if(gameState === 0){
+      player = new Player();
+      player.getCount();
+      form = new Form()
+      form.display();
+    }
+
+  }
+  play(){
+     form.hide();
+     textSize(30);
+
+     text("Game Start",120,100)
+     Player.getPlayerInfo();
+     if(allPlayers!== undefined){
+       var y = 150;
+       
+       for(var i in allPlayers ){
+         textSize(15);
+         if(i=="player"+ player.index){
+            fill("red");
+            
+         }
+         else{
+           fill(0);
+         }
+          text(allPlayers[i].name + ":" + allPlayers[i].distance,120,y)
+          y=y+20
+       }
+
+     }
+     if(keyIsDown(UP_ARROW)&& player.index!== null){
+       player.distance= player.distance+50
+       player.update();
+     }
+  }
+}
